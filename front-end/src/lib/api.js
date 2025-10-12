e92cf2319e7f0a7b13456d11c0b68b00c983c8d6
Said <briansaid2@gmail.com>
2025-10-13 00:33:49 +0300

    Finalcommit

--- front-end/src/lib/api.js
+++ front-end/src/lib/api.js
@@ -1,12 +1,8 @@
-// ../lib/api.js
+// src/lib/api.js OR src/services/api.js
 import axios from "axios";
 
-// Set your backend URL here
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // <-- Make sure this matches your backend port
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "https://terrasense-hackathon-2.onrender.com/api", // 👈 use your live backend URL
 });
 
 export default API;
